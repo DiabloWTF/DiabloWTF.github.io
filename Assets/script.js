6d7c7ac70a5a77e658fda19dd9dd73ceba13f1c5
jwe0 <jwe0@epic.x>
2024-07-20 09:42:38 +0100

    Optimized suboptimal code

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+function showAbout() {
+    document.getElementById('content').style.display = 'none';
+    document.getElementById('about-diablo').style.display = 'block';
+}
+
+function hideAbout() {
+    document.getElementById('about-diablo').style.display = 'none';
+    document.getElementById('content').style.display = 'flex';
+}
